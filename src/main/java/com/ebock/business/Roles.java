84ce482903d2f0c427a0514763162af0cee2f167
William Dubuc <william.dubuc01@gmail.com>
2026-05-14 09:35:11 -0400

    Creation du projet

---
+++ src/main/java/com/ebock/business/Roles.java
@@ -0,0 +1,9 @@
+package com.ebock.business;
+
+public final class Roles {
+    private Roles() { }
+    public static final String STUDENT = "student";
+    public static final String TEACHER = "teacher";
+    public static final String DEFAULT = "default-roles-master";
+
+}
