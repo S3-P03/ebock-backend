79fb6ffc496b134ebc129e4f5fa985c61d56a9fe
elianepelletier <157521481+elianepelletier@users.noreply.github.com>
2026-06-07 16:52:41 -0400

    ajout tests pour requêtes profil vendeur

--- src/test/java/com/ebock/TestProfilVendeur.java
+++ src/test/java/com/ebock/TestProfilVendeur.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 
 @TestSecurity(user = "pele3157", roles = {"admin"})
 @QuarkusTest
@@ -18,6 +19,27 @@ class TestProfilVendeur {
                 .get("/item/{cip}/storefront")
                 .then()
                 .statusCode(200)
-                .body("name", is("<[Prise de laptop]>"));
+                .body("[0].name", is("Prise de laptop"))
+                .body("[0].description", is("Une prise de laptop vraiment longue"))
+                .body("[0].price", is(10.0f))
+                .body("[0].quantity", is(1))
+                .body("[0].categoryId", is(2))
+                .body("[0].wearId", is(1))
+                .body("[0].firstName", is("Éliane"))
+                .body("[0].lastName", is("Pelletier"));
+    }
+
+    @Test
+    void testRequeteUser() {
+        given()
+                .pathParam("cip","pele3157")
+                .when()
+                .get("/user/{cip}/storefront")
+                .then()
+                .statusCode(200)
+                .body("firstName", is("Éliane"))
+                .body("lastName", is("Pelletier"))
+                .body("profilePictureUrl", is(nullValue()))
+                .body("createdAt", is("2026-06-07 17:41:32.568706"));
     }
 }
