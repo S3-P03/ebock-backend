6226bcf89a65aa55bc3bad5159cbaff998bd8cae
William Dubuc <william.dubuc01@gmail.com>
2026-06-10 10:44:45 -0400

    Added image in DB

--- src/test/java/com/ebock/item/ProfilVendeurIT.java
+++ src/test/java/com/ebock/item/ProfilVendeurIT.java
@@ -1,7 +1,6 @@
-package com.ebock;
+package com.ebock.item;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.security.TestSecurity;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
