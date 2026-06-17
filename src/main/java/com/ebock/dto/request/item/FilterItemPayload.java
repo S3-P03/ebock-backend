cb76e1ff98eab21037a1d4bdbb1f41008d430364
elianepelletier <157521481+elianepelletier@users.noreply.github.com>
2026-06-17 16:38:16 -0400

    début de requête fonctionnelle
    la requête réussit à filtrer
    il manque les favoris et le fait qu'on veut que la requête retourne 25 items max

--- src/main/java/com/ebock/dto/request/item/FilterItemPayload.java
+++ src/main/java/com/ebock/dto/request/item/FilterItemPayload.java
@@ -3,14 +3,51 @@ package com.ebock.dto.request.item;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+
 public class FilterItemPayload {
     BigDecimal minPrice;
     BigDecimal maxPrice;
     int maxDistance;
-    Boolean favorite;
+    Boolean favorite; //à ajouterrrr
     ArrayList<Integer> listCategoryId;
     ArrayList<Integer> listTagId; //plusieurs dans item
     ArrayList<Integer> listWearId;
     ArrayList<Integer> listDeliveryId; //plusieurs dans item
     ArrayList<Integer> listPaymentId; //plusieurs dans item
+
+    public BigDecimal getMinPrice() {
+        return minPrice;
+    }
+
+    public BigDecimal getMaxPrice() {
+        return maxPrice;
+    }
+
+    public int getMaxDistance() {
+        return maxDistance;
+    }
+
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    public ArrayList<Integer> getListCategoryId() {
+        return listCategoryId;
+    }
+
+    public ArrayList<Integer> getListTagId() {
+        return listTagId;
+    }
+
+    public ArrayList<Integer> getListWearId() {
+        return listWearId;
+    }
+
+    public ArrayList<Integer> getListDeliveryId() {
+        return listDeliveryId;
+    }
+
+    public ArrayList<Integer> getListPaymentId() {
+        return listPaymentId;
+    }
 }
