28d9e0c1129525fa0919d7cb498a837cc6cd51a8
Jeef <124377533+JeanFelixLarouche@users.noreply.github.com>
2026-06-23 21:55:44 -0400

    Removed unused context

--- src/main/java/com/ebock/service/DeliveryOptionService.java
+++ src/main/java/com/ebock/service/DeliveryOptionService.java
@@ -7,9 +7,7 @@ import com.ebock.mapper.DeliveryOptionMapper;
 import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.SecurityContext;
 
 import java.util.List;
 
@@ -21,8 +19,6 @@ public class DeliveryOptionService {
     DeliveryOptionMapper deliveryMapper;
     @Inject
     DeliveryOptionConverter deliveryConverter;
-    @Context
-    SecurityContext securityContext;
 
     @GET
     @Path("/list/")
