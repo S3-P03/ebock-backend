f352ecd2c9cc6173c7fd06141bf51e4baac24ec0
elianepelletier <157521481+elianepelletier@users.noreply.github.com>
2026-07-03 16:19:17 -0400

    ajout vérification d'une string

--- src/main/java/com/ebock/dto/request/review/ReviewPayload.java
+++ src/main/java/com/ebock/dto/request/review/ReviewPayload.java
@@ -2,8 +2,12 @@ package com.ebock.dto.request.review;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class ReviewPayload {
+    @NotBlank
+    @Size(max=50)
     public String content;
     @Min(1)
     @Max(5)
