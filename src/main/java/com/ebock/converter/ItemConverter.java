08b0ee729ae2957dac4582995ee0814d1075a0c2
William Dubuc <william.dubuc01@gmail.com>
2026-07-05 18:27:47 -0400

    Remove warning + mini fix city

--- src/main/java/com/ebock/converter/ItemConverter.java
+++ src/main/java/com/ebock/converter/ItemConverter.java
@@ -6,14 +6,34 @@ import com.ebock.dto.request.item.ItemUpdatePayload;
 import com.ebock.dto.response.item.ItemInsertResponse;
 import com.ebock.dto.response.item.ItemResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "cdi")
 public interface ItemConverter {
+    @Mapping(target = "firstName", ignore = true)
+    @Mapping(target = "lastName", ignore = true)
+    @Mapping(target = "favorite", ignore = true)
+    @Mapping(target = "tags", ignore = true)
+    @Mapping(target = "firstImage", ignore = true)
     ItemResponse toResponse(Item item);
     List<ItemResponse> toResponse(List<Item> list);
+
+    @Mapping(target = "itemId", ignore = true)
+    @Mapping(target = "addedAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "sold", ignore = true)
+    @Mapping(target = "archived", ignore = true)
+    @Mapping(target = "sellerCip", ignore = true)
     Item toBusiness(ItemCreatePayload payload);
+
+    @Mapping(target = "itemId", ignore = true)
+    @Mapping(target = "addedAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "sold", ignore = true)
+    @Mapping(target = "archived", ignore = true)
+    @Mapping(target = "sellerCip", ignore = true)
     Item toBusiness(ItemUpdatePayload payload);
     ItemInsertResponse toInsertResponse(Item item);
 }
--- src/main/java/com/ebock/converter/TagConverter.java
+++ src/main/java/com/ebock/converter/TagConverter.java
@@ -4,6 +4,7 @@ import com.ebock.business.Tag;
 import com.ebock.dto.request.tag.TagPayload;
 import com.ebock.dto.response.tag.TagResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -11,6 +12,9 @@ import java.util.List;
 public interface TagConverter {
     TagResponse toResponse(Tag tags);
     List<TagResponse> toResponse(List<Tag> tags);
+
+    @Mapping(target = "tagId", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
     Tag toBusiness(TagPayload payload);
 }
 
