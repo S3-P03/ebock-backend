b2199ab6e73f93ac8980699d64011024e1863b45
William Dubuc <william.dubuc01@gmail.com>
2026-07-12 22:09:27 -0400

    OpenApi qui marche enfin

--- src/main/java/com/ebock/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/ebock/infrastructure/config/OpenApiConfig.java
@@ -2,11 +2,8 @@ package com.ebock.infrastructure.config;
 
 import jakarta.ws.rs.core.Application;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
-import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
-import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -14,17 +11,8 @@ import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
                 version = "1.0.0"
         ),
         security = {
-                @SecurityRequirement(name = "Environment"),
                 @SecurityRequirement(name = "SecurityScheme")
         }
 )
-@SecurityScheme(
-        securitySchemeName = "Environment",
-        type = SecuritySchemeType.APIKEY,
-        in = SecuritySchemeIn.HEADER,
-        apiKeyName = "Environment",
-        description = "'ebock' ou 'dark_ebock'"
-)
 public class OpenApiConfig extends Application {
-
 }
